bfeb664d43da66bbffa27a052e68db6aaec30088
kasturi28 <kasturipansare2818@gmail.com>
2022-05-22 07:42:23 +0530

    Day3LineComparison.java

---
+++ Day3.java
@@ -0,0 +1,10 @@
+
+package EmployeeWageCalculation;
+
+
+public class EmployeeWageCalculation {
+
+    public static void main (String[] args) {
+        System.out.println("WELCOME TO ALL THE EMPLOYEES OF HIRECT");
+    }
+}
